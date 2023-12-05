665fdeba28028921c33cf3533deb4b7774b4cac1
Eduardo Santana <eduardo.silva@financas360.com.br>
2023-12-05 14:54:38 -0300

    Modificado script para criar uma pasta com os arquivos filtrados

--- search_word_file.py
+++ search_word_file.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 
 def search_in_file(file_path, text):
     try:
@@ -13,22 +14,27 @@ def search_in_file(file_path, text):
             print(f"Could not decode file: {file_path}")
             return False
 
-def search_in_files(directory: str, extension: str, text: str):
+def search_in_files(directory: str, extension: str, text: str, target_directory: str):
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.' + extension):
                 file_path = os.path.join(root, file)
                 if search_in_file(file_path, text.lower()):
                     print(f"Text found in: {file_path}")
+                    shutil.copy2(file_path, os.path.join(target_directory, file))
 
 def main():
     parser = argparse.ArgumentParser(description="Text for a text in all files with a specific extension.")
     parser.add_argument("directory", type=str, help="Directory to search in")
     parser.add_argument("extension", type=str, help="File extension to look for")
     parser.add_argument("text", type=str, help="Text to search for")
+    parser.add_argument("target_directory", type=str, help="Directory to copy files to")
     args = parser.parse_args()
 
-    search_in_files(args.directory, args.extension, args.text)
+    search_in_files(args.directory, args.extension, args.text, args.target_directory)
 
 if __name__ == "__main__":
     main()
