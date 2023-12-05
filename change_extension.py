f48b99b41deb0447c965c3c0f96eb1ad081c8abd
eduardo.silva@financas360.com.br <edu@F360-NB0351.localdomain>
2023-12-05 14:28:29 -0300

    Criado script para adicionar uma extensao em todos arquivos numa pasta

---
+++ change_extension.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+
+def add_extension_to_files(directory: str, extension: str) -> None:
+    for filename in os.listdir(directory):
+        if not filename.endswith(extension):
+            new_filename = f"{filename}.{extension}"
+            os.rename(os.path.join(directory, filename), 
+                      os.path.join(directory, new_filename))
+            print(f"Renamed {filename} to {new_filename}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Add extension to all files in a directory.")
+    parser.add_argument("directory", type=str, help="Directory to process")
+    parser.add_argument("extension", type=str, help="Extension to add")
+    args = parser.parse_args()
+
+    add_extension_to_files(args.directory, args.extension)
+
+if __name__ == "__main__":
+    main()
